754e965e4e8c0b65192f38f892b01fecf16c8599
VortexFeri <fericarnu@gmail.com>
2024-04-24 23:20:39 +0300

    Refactored Pump and sensor logic
    - Now uses Feri's Callback Timer to set sensor polling intervals
    - The pump's logic has been encapsulated into a struct, which is responsible of turning itself off by setting a TimeOut callback
    - Display and Internet features are temporarely removed

--- src/main_sketch/config.h
+++ src/main_sketch/config.h
@@ -1,29 +1,25 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const uint8_t pump_out PROGMEM = 7; // gate signal to pump transistor 
-const uint8_t light_out PROGMEM = 6; // power the DIY LED strip
+namespace config {
+  const uint8_t pump_out PROGMEM = 7; // gate signal to pump transistor 
+  const uint8_t light_out PROGMEM = 6; // power the DIY LED strip
 
-const uint8_t light_readPin PROGMEM = A3; // light sensor (photoresistor) input pin 
-const uint8_t moistureSensor_readPin PROGMEM = A4; // moisture sensor (DIY) input pin
-const uint8_t temperatureSensor_readPin PROGMEM = 5; // digital temperature sensor input pin
+  const uint8_t light_readPin PROGMEM = A3; // light sensor (photoresistor) input pin 
+  const uint8_t moistureSensor_readPin PROGMEM = A4; // moisture sensor (DIY) input pin
+  const uint8_t temperatureSensor_readPin PROGMEM = 5; // digital temperature sensor input pin
 
-const short sensor_pollRate = 2000; // interval (in ms) at which the sensor values are updated
+  const short sensor_pollRate = 2000; // interval (in ms) at which the sensor values are updated
 
-const uint8_t moisture_threshold PROGMEM = 60; // under this limit (in percents [%]), the the pump is turned on
-const uint8_t light_threshold PROGMEM = 15; // under this limit (in percents [%]), the led strip is turned on
-const int minEmailInterval = 30000; // time (in ms) after which we try to send an e-mail if there is no water
+  const uint8_t moisture_threshold PROGMEM = 60; // under this limit (in percents [%]), the the pump is turned on
+  const uint8_t light_threshold PROGMEM = 15; // under this limit (in percents [%]), the led strip is turned on
+  const int minEmailInterval = 30000; // time (in ms) after which we try to send an e-mail if there is no water
 
-unsigned long timeSinceLastMail = 0;
-unsigned long sensor_lastPoll = 0; 
-unsigned long pump_lastStart = 0;
+  unsigned long timeSinceLastMail = 0;
+  unsigned long sensor_lastPoll = 0; 
+  unsigned long pump_lastStart = 0;
 
-const short pump_coolDown = 20000; // cooldown (ms) until the pump can start again (to give the plant time to absorb the water)
-const short pump_activeDuration = 1000; // amount of time (in ms) for which the pump is on before stopping
-
-uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // MAC address from which to send the mail HTTP request
-
-const char HOST_NAME[] = "maker.ifttt.com";
-const char PATH_NAME[] = "/trigger/send-email/with/key/heQXcCZGAUOnHDjYqOk7d2zmYVCYitI7pZkzSlRJC-B";
-
+  const short pump_coolDown = 20000; // cooldown (ms) until the pump can start again (to give the plant time to absorb the water)
+  const short pump_activeDuration = 1000; // amount of time (in ms) for which the pump is on before stopping
+}
 #endif // end config.h
--- src/main_sketch/main_sketch.ino
+++ src/main_sketch/main_sketch.ino
@@ -1,138 +1,78 @@
-#include <SPI.h>
-#include <Ethernet.h>
-
-#include "Ucglib.h"
-
+#include <timer.h>
 #include "config.h"
-#include "display.h"
-#include "network.h"
+#include <Ucglib.h>
 
-// sensor variables
 float moisture = 0;
-uint8_t light = -1;
-bool isWarm = false;
+uint8_t light = 0;
 
-bool internetAvailable;
+Timer sensorTimer;
+
+struct Pump {
+  bool isOnCooldown;
+  Timer timer;
+
+  void turnOn() {
+    PORTD = (1 << config::pump_out) | PORTD;
+    instance().timer.setTimeout(config::pump_activeDuration, [] {
+      PORTD = ~(1 << config::pump_out) & PORTD;
+      Serial.println(F("Pump turned ON"));
+    });
+    isOnCooldown = true;
+    instance().timer.setTimeout(config::pump_coolDown, [] {
+      instance().isOnCooldown = false;
+      Serial.println(F("Pump turned OFF"));
+    });
+  }
+
+private:
+  // Helper function to get the instance of the class, as we cannot capture anything in timer's lambda
+  static Pump& instance() {
+    static Pump pumpInstance;
+    return pumpInstance;
+  }
+} pump;
+
+void pollSensors() {
+  // resistance decreases when wet, so subtract it from max (1023)
+  moisture = ((1023.0 - analogRead(config::moistureSensor_readPin)) / 1023.0) * 100.0;
+  // resistance increases with light
+  light = ((analogRead(config::light_readPin)) / 1023.0) * 100.0;
+
+  Serial.print(F("Moisture: "));
+  Serial.print(moisture);
+  Serial.print(F("%"));
+
+  Serial.print(F(" | Light: "));
+  Serial.print(light);
+  Serial.println(F("%"));
+}
 
 void setup() {
   Serial.begin(9600);
 
   // configure pins
-  DDRD = (1 << light_out) | (1 << pump_out);
-  PORTD = 1 << temperatureSensor_readPin;
+  DDRD = (1 << config::light_out) | (1 << config::pump_out);
+  PORTD = 1 << config::temperatureSensor_readPin;
 
-  pinMode(moistureSensor_readPin, INPUT);
-  pinMode(light_readPin, INPUT);
+  pinMode(config::moistureSensor_readPin, INPUT);
+  pinMode(config::light_readPin, INPUT);
 
-  // initialize display
-  ucg.begin(0);
-  ucg.clearScreen();
-
-  // make screen black
-  ucg.setColor(0,0,0);
-  ucg.drawBox(4,0, ucg.getWidth() + 4, ucg.getHeight());
-
-  ucg.setColor(255, 255, 255);
-  ucg.setPrintPos(4, 16);
-  ucg.setFont(ucg_font_helvB08_tr);
-  ucg.println(F("Connecting to internet..."));
-
-  // try to connect to the Internet (timeout = 10s)
-  internetAvailable = Ethernet.begin(mac, 10000);
-  ucg.setColor(0,0,0);
-  ucg.drawBox(0,0, ucg.getWidth() + 4, ucg.getHeight() - LAN_ICON_H - 4);
-
-  // handle connection failure
-  if (!internetAvailable) {
-    Serial.println(F("Failed to obtain an IP address using DHCP"));
-    displayNoInternet(2, true); // display an icon scaled x2
-  }
+  sensorTimer.setInterval(config::sensor_pollRate, pollSensors);
 }
 
 void loop() {
-  // get the time in ms since the program started
-  const unsigned long currentTime = millis();
+  // call tick on all timers so they update at each loop iteration
+  sensorTimer.tick();
+  pump.timer.tick();
 
-  const uint8_t text_height = 14; // how many pixels to go down when printing anouther text row
-  uint8_t pos[2] = {6, text_height};
-  uint8_t color[3];
-
-  // check the sensor once at every <sensor_pollrate> ms
-  if (currentTime - sensor_lastPoll >= sensor_pollRate) {
-    // get percent values for moisture and light level
-    moisture = ((1023.0 - analogRead(moistureSensor_readPin)) / 1023.0) * 100.0; // resistance decreases when wet, so subtract it from max (1023)
-    light = ((analogRead(light_readPin)) / 1023.0) * 100.0; // resistance increases with light
-
-    // read temperature sensor from PIND readPin 
-    isWarm = !((PIND & (1 << temperatureSensor_readPin)) >> temperatureSensor_readPin); // HIGH = cold; LOW = warm
-    
-    Serial.print(F("Moisture: "));
-    Serial.print(moisture);
-    Serial.print(F("%"));
-
-    Serial.print(F(" | Light: "));
-    Serial.print(light);
-    Serial.print(F("%"));
-
-    Serial.print(F(" | Temp: "));
-    Serial.println(isWarm ? "Warm" : "Cold");
-
-    sensor_lastPoll = currentTime; // keep track of this sensor polling
-
-    color[0] = 0;
-    color[1] = moisture <= moisture_threshold ? 0 : 255; // green if moist
-    color[2] = moisture > moisture_threshold ? 0 : 255; // red if dry
-    displaySensorValue(pos, color, "Moisture", moisture, "%");
-    
-    pos[1] += text_height;
-    color[0] = 0;
-    color[1] = light < light_threshold ? 0 : 255; // green if well lit
-    color[2] = light >= light_threshold ? 0 : 255; // red if dark
-    displaySensorValue(pos, color, "Light level", light, "%", true);
-
-    pos[1] += text_height;
-    color[0] = !isWarm ? 255 : 0; // blue if cold
-    color[1] = isWarm ? 255 : 0; // green if warm
-    color[2] = 0;
-    displaySensorValue(pos, color, "Temp", isWarm ? "Warm" : "Cold");
-
-    // happy face if moist, sad face if dry
-    displayFace(3, moisture > moisture_threshold);
+  if (!pump.isOnCooldown && moisture < config::moisture_threshold) {
+    pump.turnOn();
   }
 
-  // turn on the pump if the soil is dry and the pump is noton cooldown
-  if (moisture <= moisture_threshold && currentTime - pump_lastStart >= pump_coolDown) {
-    // digitalWrite(pump_pin, HIGH);
-    PORTD = (1 << pump_out) | PORTD;
-    pump_lastStart = currentTime;
-    
-    Serial.print(F("Pump turned ON at: "));
-    Serial.println(currentTime);
-  }
-
-  // turn the pump off after pump_activeDuration has passed
-  int isPumpOn = (PIND & (1 << pump_out)) >> pump_out;
-  if (isPumpOn && currentTime - pump_lastStart >= pump_activeDuration) {
-    // digitalWrite(pump_pin, LOW);
-    PORTD = ~(1 << pump_out) & PORTD;
-    Serial.print(F("Pump turned OFF at: "));
-    Serial.println(currentTime);
-  }
-
-  // turn on the led strip if dark, otherwise turn it off 
-  if (light != -1 && light < light_threshold) {
-    // digitalWrite(light_out, HIGH); 
-    PORTD = (1 << light_out) | PORTD;
+  // turn on the led strip if dark, otherwise turn it off
+  if (light != -1 && light < config::light_threshold) {
+    PORTD = (1 << config::light_out) | PORTD;
   } else {
-    // digitalWrite(light_out, LOW);
-    PORTD = ~(1 << light_out) & PORTD;
-  }
-
-  // try to send an e-mail if the soil has been dry for a while
-  if (moisture <= moisture_threshold && pump_lastStart - timeSinceLastMail >= minEmailInterval) {
-    Serial.println(F("Trying to send e-mail!"));
-    // try to send an emai; and if it fails, show an icon telling there is no internet
-    displayNoInternet(2, !sendEmail(moisture, light, isWarm));
-    timeSinceLastMail = currentTime;
+    PORTD = ~(1 << config::light_out) & PORTD;
   }
 }
