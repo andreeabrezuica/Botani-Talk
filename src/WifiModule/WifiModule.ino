9457dada30eb17028c9cbd0adc9db3d287bed31a
VortexFeri <fericarnu@gmail.com>
2024-05-06 00:24:39 +0300

    #10 #14 implemented stat history (csv file)

--- src/WifiModule/WifiModule.ino
+++ src/WifiModule/WifiModule.ino
@@ -2,16 +2,33 @@
 #include <WiFiManager.h>
 #include <SerialTransfer.h>
 #include <LittleFS.h>
+#include "time.h"
+
+#include <CSV_Parser.h>
+
+#include "CallbackTimer.h"
 
 // select which pin will trigger the configuration portal when set to LOW
 #define TRIGGER_PIN D2
-
-#define SW_SERIAL 0
-#define HW_SERIAL 1
+#define FILE_READ_FAILED "Failed to open file for reading"
 
 int timeout = 120;  // timeout for configuration portal
 SerialTransfer transfer;
 WiFiServer server(80);
+Timer serialTimer;
+bool connected;
+bool gotTimeFromNTP;
+Timer saveTimer;
+
+unsigned long unixTime;
+
+struct __attribute__((packed)) PS {
+  float temp;
+  int moist;
+  int light;
+  bool lights_on;
+  bool pump_on;
+} plantStatus;
 
 void setup() {
   WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
@@ -24,16 +41,62 @@ void setup() {
   WiFiManager wm;
   // Automatically connect using saved credentials,
   // if connection fails, it starts an access point with the specified name
-  bool res = wm.autoConnect("BotaniTalk-AP");
-
-  if (res) {
+  if (wm.autoConnect("BotaniTalk-AP")) {
+    connected = true;
     startServer();
+    getUnixTime();
   } else {
     Serial.println("Failed to connect");
   }
+
+  // TEST
+  serialTimer.setInterval(1000, [] {
+    plantStatus.light = (plantStatus.light + 13) % 100;
+    plantStatus.moist = (plantStatus.moist + 5) % 100;
+    plantStatus.temp = (plantStatus.temp + .5);
+    if (plantStatus.temp > 30) {
+      plantStatus.temp = 10;
+    }
+  });
+  saveTimer.setInterval(60000, updateCSV);
+}
+
+void updateCSV() {
+  Serial.println("Saving CSV file");
+  getInfoFromMega();
+  
+  String values = String("\n") + plantStatus.moist + "," + plantStatus.light + "," + plantStatus.temp + "," + getUnixTime();
+  if (readFile("/sensors.csv").isEmpty()) {
+    appendFile("/sensors.csv", "moisture,light,temperature,time\n");
+  }
+  appendFile("/sensors.csv", values.c_str());
+}
+
+unsigned long getUnixTime() {
+  if (connected && !gotTimeFromNTP) {
+    time_t now;
+    struct tm timeInfo;
+    configTime(10800, 0, "pool.ntp.org");
+    if (!getLocalTime(&timeInfo)) {
+      Serial.println("Failed to obtain time");
+      return false;
+    }
+    gotTimeFromNTP = true;
+    time(&now);
+    unixTime = now;
+    return now;
+  }
+  if (gotTimeFromNTP) {
+    unixTime += (millis() / 1000);
+    return unixTime;
+  }
+  return 0;
 }
 
 void loop() {
+  serialTimer.tick();
+  saveTimer.tick();
+
   if (digitalRead(TRIGGER_PIN) == LOW) {
     startPortal();
   }
@@ -54,9 +117,20 @@ void loop() {
       sendInfo(client);
     } else if (request.indexOf("/style.css") != -1) {
       sendCSSFile(client);
+    } else if (request.indexOf("/sensors") != -1) {
+      sendHistory(client);
     } else {
       sendHomePage(client);
     }
+  }
+}
+
+void getInfoFromMega() {
+  char arr[] = "-info";
+  transfer.sendDatum(arr);
+
+  while (receiveSerial(plantStatus)) {
+    Serial.println("Receiving data...");
   }
 }
 
@@ -70,14 +144,27 @@ void sendResponseHeader(WiFiClient& client, const String& contentType, size_t co
 }
 
 void sendInfo(WiFiClient& client) {
-  // TEMPORARY
-  const String response = "{\"moist\": 87, \"light\": 60, \"temp\": 25.4}";
+  // request information from Arduino Mega
+  getInfoFromMega();
+  const String response = String("{\"moist\":") + plantStatus.moist + String(",\"light\":") + plantStatus.light + String(",\"temp\":") + plantStatus.temp + String(",\"lights_on\":") + plantStatus.lights_on + String(",\"pump_on\":") + plantStatus.pump_on + String("}");
   sendResponseHeader(client, "text/json; charset=utf-8", response.length());
   client.println(response);
 }
 
+void sendHistory(WiFiClient& client) {
+  const String response = readFile("/sensors.csv");
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/csv");
+  client.println("Content-Length: " + response.length());
+  client.println("Access-Control-Allow-Origin: http:://127.0.0.1:80");
+  client.println("Connection: close");
+  client.println();
+
+  client.println(response);
+}
+
 void sendHomePage(WiFiClient& client) {
-  const String fileContent = ReadFileToString("/index.html");
+  const String fileContent = readFile("/index.html");
   if (!fileContent.isEmpty()) {
     sendResponseHeader(client, "text/html; charset=utf-8", fileContent.length());
     client.println(fileContent);
@@ -87,7 +174,7 @@ void sendHomePage(WiFiClient& client) {
 }
 
 void sendCSSFile(WiFiClient& client) {
-  const String fileContent = ReadFileToString("/style.css");
+  const String fileContent = readFile("/style.css");
   if (!fileContent.isEmpty()) {
     sendResponseHeader(client, "text/css; charset=utf-8", fileContent.length());
     client.println(fileContent);
@@ -104,38 +191,38 @@ void send404(WiFiClient& client) {
   client.println("404 Not Found");
 }
 
-String ReadFileToString(const char* filename) {
+String readFile(const char* filename) {
   if (!LittleFS.begin()) {
     return String("An Error has occurred while mounting LittleFS");
   }
 
   File file = LittleFS.open(filename, "r");
   if (!file) {
-    return String("Failed to open file for reading");
+    return String(FILE_READ_FAILED);
   }
   String data = file.readString();
   file.close();
   return data;
 }
 
-void transmitSerial(const char* buff, bool softwareSerial) {
-  if (softwareSerial) {
-    transfer.txObj(buff, sizeof(buff));
-    transfer.sendData(sizeof(buff));
-  } else {
-    Serial.write(buff);
+bool appendFile(const char* filename, const char* message) {
+  if (!LittleFS.begin()) {
+    return false;
   }
+  File file = LittleFS.open(filename, "a");
+  if (!file) {
+    return false;
+  }
+  bool success = file.print(message);
+  file.close();
+  return success;
 }
 
-void receiveSerial(char* buff, bool softwareSerial) {
-  if (!softwareSerial) {
-    String s = Serial.readString();
-    strcpy_P(buff, s.c_str());
-    Serial.print(buff);
-    return;
-  }
+template<typename T>
+bool receiveSerial(T& buff) {
   if (transfer.available()) {
-    transfer.rxObj(buff, sizeof(buff));
+    transfer.rxObj(buff);
+    return true;
   } else if (transfer.status < 0) {
     Serial.print("ERROR: ");
     switch (transfer.status) {
@@ -150,6 +237,7 @@ void receiveSerial(char* buff, bool softwareSerial) {
         break;
     }
   }
+  return false;
 }
 
 void startPortal() {
@@ -167,6 +255,7 @@ void startPortal() {
 
   //if you get here you have connected to the WiFi
   Serial.println("CONNECTED");
+  connected = true;
   startServer();
 }
 
